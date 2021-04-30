3d70b78f03c7f8d55a37a5896ccec7e7d4432e89
Gerardo Peart <31547267+GerardoPeart@users.noreply.github.com>
2021-04-30 16:17:46 -0500

    Add files via upload

---
+++ api/Tabledata.js
@@ -0,0 +1,11 @@
+var tables = [
+    {
+      Name: "Gera",
+      Email: "gera.peart@gmail.com",
+      id: "1",
+      phone: "111111111"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tables;
---
+++ api/waitlistdata.js
@@ -0,0 +1,12 @@
+var wtables = [
+    {
+        Name: "adrian",
+        Email: "adrian.peart@gmail.com",
+        id: "0",
+        phone: "2222222222"
+    }
+  ];
+  
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = wtables;
